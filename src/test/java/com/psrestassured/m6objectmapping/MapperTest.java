ca15d9fc5ef860a932356a8673b80ddc9df11331
C1324634 Ricardo Carteri Uehlein <c1324634@interno.bb.com.br>
2022-07-18 11:19:45 -0300

    6. Adding a mapper to the project

---
+++ src/test/java/com/psrestassured/m6objectmapping/MapperTest.java
@@ -0,0 +1,10 @@
+package com.psrestassured.m6objectmapping;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class MapperTest {
+    public static void main(String[] args) {
+        ObjectMapper mapper = new ObjectMapper();
+
+    }
+}
